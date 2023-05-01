b5008934f569dcb823c6e8ff74248b6fcb938cc5
Wild Fly <olitieplove@gmail.com>
2023-05-01 21:50:29 +0300

    fix: press different shift, ctrl, alt

--- script.js
+++ script.js
@@ -37,89 +37,95 @@ function feelWithKeys () {
         for (let i = 0; i < line.length; i++) {
             if (line[i] === 9) {
                 out += '<div class="clearfix"></div>';
-                out += '<div class="keyboard-key tab" data="'+ line[i]+'"> Tab </div>';
+                out += '<div class="keyboard-key tab" data="'+ line[i] +'"> Tab </div>';
             } else if (line[i] === 20) {
                 out += '<div class="clearfix"></div>';
-                out += '<div class="keyboard-key capslock" data="'+ line[i]+'"> Capslock </div>';
+                out += '<div class="keyboard-key capslock" data="'+ line[i] +'"> Capslock </div>';
             } else if (line[i] === 16) {
                 if (shiftCount === 0) {
                     out += '<div class="clearfix"></div>';
                     shiftCount++;
+                    out += '<div class="keyboard-key shift-left" data="'+ line[i] +'"> Shift </div>';
+                } else {
+                    out += '<div class="keyboard-key shift-right" data="'+ line[i] +'"> Shift </div>';
                 }
-                out += '<div class="keyboard-key shift" data="'+ line[i]+'"> Shift </div>';
             } else if (line[i] === 17) {
                 if (ctrlCount === 0) {
                     out += '<div class="clearfix"></div>';
                     ctrlCount++;
+                    out += '<div class="keyboard-key ctrl-left" data="'+ line[i] +'"> Ctrl </div>';
+                } else {
+                    out += '<div class="keyboard-key ctrl-right" data="'+ line[i] +'"> Ctrl </div>';
                 }
-                out += '<div class="keyboard-key ctrl" data="'+ line[i]+'"> Ctrl </div>';
             } else if (line[i] === 18) {
                 if (altCount === 0) {
                     altCount++;
+                    out += '<div class="keyboard-key alt-left" data="'+ line[i] +'"> Alt </div>';
+                } else { 
+                    out += '<div class="keyboard-key alt-right" data="'+ line[i] +'"> Alt </div>';
                 }
-                out += '<div class="keyboard-key alt" data="'+ line[i]+'"> Alt </div>';
             } else if (line[i] === 91) {
-                out += '<div class="keyboard-key win" data="'+ line[i]+'"> Win </div>';
+                out += '<div class="keyboard-key win" data="'+ line[i] +'"> Win </div>';
             } else if (line[i] === 37) {
-                out += '<div class="keyboard-key" data="'+ line[i]+'"> ← </div>';
+                out += '<div class="keyboard-key" data="'+ line[i] +'"> ← </div>';
             } else if (line[i] === 38) {
-                out += '<div class="keyboard-key" data="'+ line[i]+'"> ↑ </div>';
+                out += '<div class="keyboard-key" data="'+ line[i] +'"> ↑ </div>';
             } else if (line[i] === 39) {
-                out += '<div class="keyboard-key" data="'+ line[i]+'"> → </div>';
+                out += '<div class="keyboard-key" data="'+ line[i] +'"> → </div>';
             } else if (line[i] === 40) {
-                out += '<div class="keyboard-key" data="'+ line[i]+'"> ↓ </div>';
+                out += '<div class="keyboard-key" data="'+ line[i] +'"> ↓ </div>';
             } else if (line[i] === 192) {
-                out += '<div class="keyboard-key" data="'+ line[i]+'"><span>~</span><span>`</span></div>';
+                out += '<div class="keyboard-key" data="'+ line[i] +'"><span>~</span><span>`</span></div>';
             } else if (line[i] === 49) {
-                out += '<div class="keyboard-key" data="'+ line[i]+'"><span>!</span><span>1</span></div>';
+                out += '<div class="keyboard-key" data="'+ line[i] +'"><span>!</span><span>1</span></div>';
             } else if (line[i] === 50) {
-                out += '<div class="keyboard-key" data="'+ line[i]+'"><span>@</span><span>2</span></div>';
+                out += '<div class="keyboard-key" data="'+ line[i] +'"><span>@</span><span>2</span></div>';
             } else if (line[i] === 51) {
-                out += '<div class="keyboard-key" data="'+ line[i]+'"><span>#</span><span>3</span></div>';
+                out += '<div class="keyboard-key" data="'+ line[i] +'"><span>#</span><span>3</span></div>';
             } else if (line[i] === 52) {
-                out += '<div class="keyboard-key" data="'+ line[i]+'"><span>$</span><span>4</span></div>';
+                out += '<div class="keyboard-key" data="'+ line[i] +'"><span>$</span><span>4</span></div>';
             } else if (line[i] === 53) {
-                out += '<div class="keyboard-key" data="'+ line[i]+'"><span>%</span><span>5</span></div>';
+                out += '<div class="keyboard-key" data="'+ line[i] +'"><span>%</span><span>5</span></div>';
             } else if (line[i] === 54) {
-                out += '<div class="keyboard-key" data="'+ line[i]+'"><span>^</span><span>6</span></div>';
+                out += '<div class="keyboard-key" data="'+ line[i] +'"><span>^</span><span>6</span></div>';
             } else if (line[i] === 55) {
-                out += '<div class="keyboard-key" data="'+ line[i]+'"><span>&</span><span>7</span></div>';
+                out += '<div class="keyboard-key" data="'+ line[i] +'"><span>&</span><span>7</span></div>';
             } else if (line[i] === 56) {
-                out += '<div class="keyboard-key" data="'+ line[i]+'"><span>*</span><span>8</span></div>';
+                out += '<div class="keyboard-key" data="'+ line[i] +'"><span>*</span><span>8</span></div>';
             } else if (line[i] === 57) {
-                out += '<div class="keyboard-key" data="'+ line[i]+'"><span>(</span><span>9</span></div>';
+                out += '<div class="keyboard-key" data="'+ line[i] +'"><span>(</span><span>9</span></div>';
             } else if (line[i] === 48) {
-                out += '<div class="keyboard-key" data="'+ line[i]+'"><span>)</span><span>0</span></div>';
+                out += '<div class="keyboard-key" data="'+ line[i] +'"><span>)</span><span>0</span></div>';
             } else if (line[i] === 189) {
-                out += '<div class="keyboard-key" data="'+ line[i]+'"><span>_</span><span>-</span></div>';
+                out += '<div class="keyboard-key" data="'+ line[i] +'"><span>_</span><span>-</span></div>';
             } else if (line[i] === 187) {
-                out += '<div class="keyboard-key" data="'+ line[i]+'"><span>+</span><span>=</span></div>';
+                out += '<div class="keyboard-key" data="'+ line[i] +'"><span>+</span><span>=</span></div>';
             } else if (line[i] === 32) {
-                out += '<div class="keyboard-key space" data="'+ line[i]+'"></div>';
+                out += '<div class="keyboard-key space" data="'+ line[i] +'"></div>';
             } else if (line[i] === 13) {
-                out += '<div class="keyboard-key enter" data="'+ line[i]+'">Enter</div>';
+                out += '<div class="keyboard-key enter" data="'+ line[i] +'">Enter</div>';
             } else if (line[i] === 46) {
-                out += '<div class="keyboard-key delete" data="'+ line[i]+'">Del</div>';
+                out += '<div class="keyboard-key delete" data="'+ line[i] +'">Del</div>';
             } else if (line[i] === 8) {
-                out += '<div class="keyboard-key backspace" data="'+ line[i]+'">Backspace</div>';
+                out += '<div class="keyboard-key backspace" data="'+ line[i] +'">Backspace</div>';
             } else if (line[i] === 219) {
-                out += '<div class="keyboard-key" data="'+ line[i]+'">[</div>';
+                out += '<div class="keyboard-key" data="'+ line[i] +'">[</div>';
             } else if (line[i] === 221) {
-                out += '<div class="keyboard-key" data="'+ line[i]+'">]</div>';
+                out += '<div class="keyboard-key" data="'+ line[i] +'">]</div>';
             } else if (line[i] === 220) {
-                out += '<div class="keyboard-key" data="'+ line[i]+'"><span>/</span><span>&bsol;</span></div>';
+                out += '<div class="keyboard-key" data="'+ line[i] +'"><span>/</span><span>&bsol;</span></div>';
             } else if (line[i] === 186) {
-                out += '<div class="keyboard-key" data="'+ line[i]+'">;</div>';
+                out += '<div class="keyboard-key" data="'+ line[i] +'">;</div>';
             } else if (line[i] === 222) {
-                out += '<div class="keyboard-key" data="'+ line[i]+'">&apos;</div>';
+                out += '<div class="keyboard-key" data="'+ line[i] +'">&apos;</div>';
             } else if (line[i] === 188) {
-                out += '<div class="keyboard-key" data="'+ line[i]+'">,</div>';
+                out += '<div class="keyboard-key" data="'+ line[i] +'">,</div>';
             } else if (line[i] === 190) {
-                out += '<div class="keyboard-key" data="'+ line[i]+'">.</div>';
+                out += '<div class="keyboard-key" data="'+ line[i] +'">.</div>';
             } else if (line[i] === 191) {
-                out += '<div class="keyboard-key" data="'+ line[i]+'">/</div>';
+                out += '<div class="keyboard-key" data="'+ line[i] +'">/</div>';
             } else {
-                out += '<div class="keyboard-key" data="'+ line[i]+'">' + String.fromCharCode(line[i]) + '</div>';
+                out += '<div class="keyboard-key" data="'+ line[i] +'">' + String.fromCharCode(line[i]) + '</div>';
             }
         }
         document.querySelector(".keyboard").innerHTML = out;
@@ -131,26 +137,46 @@ feelWithKeys();
 
 const textAreaContent = document.querySelector(".text-area");
 
-function light (event) {
+function highlight (event) {
     document.querySelectorAll('.keyboard .keyboard-key').forEach(el => {
         el.classList.remove('active');
     });
-    document.querySelector('.keyboard .keyboard-key[data="' + event.keyCode + '"]').classList.add("active");
+
+    if (event.shiftKey) {
+        if (event.code === 'ShiftLeft') {
+            document.querySelector('.shift-left').classList.add("active"); 
+        } else if (event.code === 'ShiftRight') {
+            document.querySelector('.shift-right').classList.add("active");
+        }
+    } else if (event.ctrlKey) {
+        if (event.code === 'ControlLeft') {
+            document.querySelector('.ctrl-left').classList.add('active'); 
+        } else if (event.code === 'ControlRight') {
+            document.querySelector('.ctrl-right').classList.add('active');
+        }
+    } else if (event.altKey) {
+        if (event.code === 'AltLeft') {
+            document.querySelector('.alt-left').classList.add('active'); 
+        } else if (event.code === 'AltRight') {
+            document.querySelector('.alt-right').classList.add('active');
+        }
+    } else {
+            document.querySelector('.keyboard .keyboard-key[data="' + event.keyCode + '"]').classList.add('active');
+        }
 }
 
 document.addEventListener("keydown", function (event) {
     textAreaContent.innerHTML += event.key;
-    light(event);
+    highlight(event);
 });
 
 document.querySelectorAll(".keyboard .keyboard-key").forEach(el => {
-    el.addEventListener("click", function (event) {
-        
+    el.addEventListener("click", function () {
         document.querySelectorAll('.keyboard .keyboard-key').forEach(el => {
             el.classList.remove('active');
         });
         this.classList.add('active');
-        let code = this.getAttribute("data");
+        let code = this.getAttribute('data');
         textAreaContent.innerHTML += String.fromCharCode(code).toLowerCase();
     });
 });
